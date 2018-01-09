ea74fca48e7d76880d70e0a1fc593843d500da66
jmfayard <jean-michel.fayard@smartmobilefactory.com>
2018-01-09 15:55:23 +0100

    Pb solved with AS 3.1.0-alpha07 / Kotlin 1.2.10

--- app/src/main/java/jmfayard/nulls/extends.kt
+++ app/src/main/java/jmfayard/nulls/extends.kt
@@ -7,8 +7,24 @@ package jmfayard.nulls
  * and let Ctrl-O - Override methods recreate them
  */
 
-/*** Actual result with Android Studio 3.0.0 stable **/
+/** Actual result with Android Studio 3.1 canary **/
+/** This is correct!!! **/
 
+class KotlinClass : JavaClass() {
+
+    override fun greeting(name: String): String {
+        return super.greeting(name)
+    }
+
+    override fun nullableSearch(param1: String?, param2: String): String? {
+        return super.nullableSearch(param1, param2)
+    }
+}
+
+
+/*** Actual result with Android Studio 3.0.0 stable **/
+/** This is wrong! **/
+/*
 class KotlinClass : JavaClass() {
 
     // name should not be String?
@@ -21,6 +37,8 @@ class KotlinClass : JavaClass() {
         return super.nullableSearch(param1, param2)
     }
 }
+*/
+
 
 /** Expected and result from Intellij 2017.5.2
 class KotlinClass : JavaClass() {
